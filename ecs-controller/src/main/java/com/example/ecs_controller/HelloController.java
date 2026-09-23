713aa205c88a8594e7e5329ef8ab923b960e617e
Ivan <ivanherrerabarrios16@gmail.com>
2026-09-23 16:57:12 -0600

    message: hola visitante

--- ecs-controller/src/main/java/com/example/ecs_controller/HelloController.java
+++ ecs-controller/src/main/java/com/example/ecs_controller/HelloController.java
@@ -10,6 +10,6 @@ public class HelloController {
 
     @GetMapping("/api/hello")
     public Map<String, String> hello(){
-        return Map.of("message", "Hola visitante");
+        return Map.of("message", "Hola humano");
     }
 }
